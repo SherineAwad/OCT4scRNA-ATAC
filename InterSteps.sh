cef6c52a31ba7fc83583768e149981c54cb1af80
Sherine Awad <sherinem@gl3126.arc-ts.umich.edu>
2024-08-01 14:48:27 -0400

    Add intermediat steps bash script

---
+++ InterSteps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Convert peaks CSV file  to bed file 
+Rscript peaksToBed.R SubsetpeaksControlsCCperSample 0
+Rscript SubsetpeaksRBPJCCperSample 0 
+
+#Use Hommer annotatePeaks.pl to annotate peaks bed file 
+annotatePeaks.pl SubsetpeaksControlsCCperSample.bed mm10 -annStats SubsetpeaksControlsCCperSample.stats > SubsetpeaksControlsCCperSampleAnnotated.bed
+annotatePeaks.pl SubsetpeaksRBPJCCperSample.bed mm10 -annStats SubsetpeaksRBPJCCperSample.stats > SubsetpeaksRBPJCCperSampleAnnotated.bed
+
+#Merge information in peaks CSV file and the annotated peaks file 
+python mergePeaksAnnotations.py SubsetpeaksControlsCCperSample.csv SubsetpeaksControlsCCperSampleAnnotated.bed > SubsetpeaksControlsCCperSampleMergedAnnotations.csv
+python mergePeaksAnnotations.py SubsetpeaksRBPJCCperSample.csv SubsetpeaksRBPJCCperSampleAnnotated.bed > SubsetpeaksRBPJCCperSampleMergedAnnotations.csv
+
+
